5d8c3c33fa331c75020d479a25206fa882df5b9b
sema <semashkodmitrij@gmail.com>
2023-08-03 22:56:26 +0300

    refactor project

--- greenJockerQuestBot-master/src/main/java/by/semashko/greenjokerquestbot/bot/handler/message/GameSessionMessageHandler.java
+++ greenJockerQuestBot-master/src/main/java/by/semashko/greenjokerquestbot/bot/handler/message/GameSessionMessageHandler.java
@@ -2,7 +2,6 @@ package by.semashko.greenjokerquestbot.bot.handler.message;
 
 import by.semashko.greenjokerquestbot.bot.BotEvent;
 import by.semashko.greenjokerquestbot.bot.handler.Handler;
-import by.semashko.greenjokerquestbot.infrastructure.scheduler.GameScheduler;
 import by.semashko.greenjokerquestbot.infrastructure.service.GameService;
 import by.semashko.greenjokerquestbot.infrastructure.service.ReplyMessageService;
 import by.semashko.greenjokerquestbot.infrastructure.service.UserService;
@@ -16,8 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Message;
-
-import java.util.concurrent.TimeUnit;
 
 @Component
 @AllArgsConstructor(onConstructor = @__ (@Autowired))
@@ -39,8 +36,11 @@ public class GameSessionMessageHandler implements Handler<Message> {
 
     @Override
     public BotApiMethod<Message> handle(Message message) {
+        Long telegramId = message.getFrom().getId();
         if (message.getText().equals("/start@GreenJokerEn_bot") || message.getText().equals("/start@GreenJokerEn_bot 1111")){
-            Long telegramId = message.getFrom().getId();
+            if (watchEngine.getGameEngineModelService().getModel().getEvent() == 0){
+                return messageService.getTextMessage(telegramId.toString(),watchEngine.getGameEngineModelService().getModel().getLevel().getTask().get(0).getText());
+            }
             watchEngine.setTelegramChatId(telegramId);
             return messageService.getTextMessage(telegramId.toString(),"Слежение за игрой включено");
         }
